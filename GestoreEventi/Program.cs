28bdb30ece7ab02454b752f06b0ef7571347b2c3
EricchiYukia <git@ericchi.moe>
2022-12-13 17:12:12 +0100

    Scommentato tutto

--- GestoreEventi/Program.cs
+++ GestoreEventi/Program.cs
@@ -1,9 +1,9 @@
 ﻿using GestoreEventi;
 
 Console.OutputEncoding = System.Text.Encoding.UTF8;
-//Evento evento1 = Management.CostruisciEvento();
-//evento1.PrenotaPosti();
-//Management.ChiediDisdette(evento1);
+Evento evento1 = Management.CostruisciEvento();
+evento1.PrenotaPosti();
+Management.ChiediDisdette(evento1);
 
 ProgrammaEventi lista1 = Management.CostruisciProgrammaEvento();
 Management.AggiungiEventiALista(lista1);
@@ -12,7 +12,7 @@ lista1.StampaListaEventi();
 
 lista1.ChiediEventiInData();
 
-// lista1.SvuotaEventi();
+lista1.SvuotaEventi();
 
 Conferenza conferenza1 = Management.CostruisciConferenza();
 conferenza1.StampaEvento();
