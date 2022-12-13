66037fc66335810b6e97eca75280aea005cc45c9
Ericchi <git@ericchi.moe>
2022-12-13 10:32:16 +0100

    Add project files.

---
+++ GestoreEventi/Evento.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestoreEventi {
+    internal class Evento {
+        private string titolo;
+        private DateOnly data;
+        private uint capienzaMassima;
+        private uint postiPrenotati;
+
+
+        public Evento(string titolo, DateOnly data, uint capienzaMassima) {
+            this.titolo = titolo;
+            this.data = data;
+            this.capienzaMassima = capienzaMassima;
+            this.postiPrenotati = 0;
+        }
+
+        public string GetTitolo() {
+            return titolo;
+        }
+
+        public void SetTitolo(string value) {
+            titolo = Utilities.ControllaVuoti(value);
+        }
+
+        public DateOnly GetData() {
+            return data;
+        }
+
+        public void SetData(DateOnly value) {
+            data = value;
+        }
+
+        public uint CapienzaMassima { get => capienzaMassima; }
+        public uint PostiPrenotati { get => postiPrenotati; }
+
+        public void PrenotaPosti(uint posti) {
+            this.postiPrenotati += posti;
+            Console.WriteLine("Sono stati prenotati " + posti + " posti.\nI posti disponibili ora sono " + (this.CapienzaMassima - this.postiPrenotati) + ".");
+        }
+
+        public void DisdiciPosti(uint posti) {
+            if (posti < this.postiPrenotati) {
+                throw new Exception("Non puoi disdire un numero di posti maggiore di quelli prenotati!");
+            }
+            if (DateOnly.FromDateTime(DateTime.Now) > data) {
+                throw new Exception("Non puoi disdire posti da un evento passato!");
+            }
+            this.postiPrenotati += posti;
+            Console.WriteLine("Sono stati prenotati " + posti + " posti.\nI posti disponibili ora sono " + (this.CapienzaMassima - this.postiPrenotati) + ".");
+        }
+
+        public void StampaEvento() {
+            Console.WriteLine("" + data.ToString() + " - " + titolo);
+        }
+    }
+}
---
+++ GestoreEventi/Utilities.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestoreEventi {
+    public class Utilities {
+
+        public static string ControllaVuoti(string value) {
+            bool valido = false;
+            while (!valido) {
+                if (value == "") {
+                    Console.Write("Hai inserito un testo vuoto. Riprova: ");
+                    value = Console.ReadLine();
+                }
+                else {
+                    valido = true;
+                }
+            }
+            return value;
+        }
+
+    }
+}
