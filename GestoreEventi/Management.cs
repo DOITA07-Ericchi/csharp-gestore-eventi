a7463d1860b4aaa59c536ea6f8673062df626581
Ericchi <git@ericchi.moe>
2022-12-13 11:44:38 +0100

    Spostato il costruiscievento in una classe Management

---
+++ GestoreEventi/Management.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestoreEventi {
+    public class Management {
+
+        public static Evento CostruisciEvento() {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Creiamo un nuovo evento!");
+            Console.Write("Nome: ");
+            string titolo = Console.ReadLine();
+            Console.Write("Data (yyyy年mm月dd日 oppure yyyy/mm/dd): ");
+            DateOnly data = Utilities.ControllaData(Console.ReadLine());
+            Console.Write("Capienza massima: ");
+            uint capienzaMassima = Utilities.ControllaUint(Console.ReadLine());
+
+            return new Evento(titolo, data, capienzaMassima);
+        }
+    }
+}
--- GestoreEventi/Program.cs
+++ GestoreEventi/Program.cs
@@ -1,11 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using GestoreEventi;
 
-Console.OutputEncoding = System.Text.Encoding.UTF8;
-Console.WriteLine("Creiamo un nuovo evento!");
-Console.Write("Nome: ");
-string titolo = Console.ReadLine();
-Console.Write("Data (yyyy年mm月dd日 oppure yyyy/mm/dd): ");
-DateOnly data = Utilities.ControllaData(Console.ReadLine());
-Console.Write("Capienza massima: ");
-uint capienzaMassima = Utilities.ControllaUint(Console.ReadLine());
+Evento evento1 = Management.CostruisciEvento();
